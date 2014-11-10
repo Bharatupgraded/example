31b454d9192cb407312f19e11e58725cb0264909
bharatupgraded <bharatupgraded@gmail.com>
2014-11-10 14:08:38 +0530

    this code will go to heroku

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ source 'https://rubygems.org'
 gem 'rails', '4.1.7'
 # Use mysql as the database for Active Record
 gem 'mysql2'
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
+end
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,20 +12,26 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.action_mailer.perform_deliveries = false #true
+  #config.action_mailer.perform_deliveries = true
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => "railscast.com",
-      :user_name => "bharatupgraded@gmail.com",
-      :password => "9248633867",
-      :authentication => "plain",
-      :enable_starttls_auto => true
-    }
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.delivery_method = :smtp
+  
+  #config.action_mailer.smtp_settings = {
+  #  :address              => 'smtp.gmail.com',
+  #  :port                 => 587,
+  #  :domain               => "gmail.com",
+  #  :user_name            => "bharatupgraded@gmail.com",
+  #  :password             => "9248633867",
+  #  :authentication       => :plain,
+  #  :enable_starttls_auto => true,
+  #  :ssl                  => true
+  #}
+
+
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -44,4 +50,20 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'railscasts.com',
+    authentication: "plain",
+    enable_starttls_auto: true,
+    ssl: true,
+    user_name: "railsdevise",
+    password: "archu123"
+  }
 end
